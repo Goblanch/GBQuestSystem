82e73f067779abd23f013c37405638934a9415d5
GoBlanch <gonzaloblanchdev@gmail.com>
2024-12-15 18:02:42 +0100

    Add window styles and basic manipulators

--- Assets/GBQuestSystem/Editor/GBQuestGraphView.cs
+++ Assets/GBQuestSystem/Editor/GBQuestGraphView.cs
@@ -3,21 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine.UIElements;
+using UnityEditor;
+using Unity.VisualScripting;
 
 namespace GBQuestSys.Windows
 {
     public class GBQuestGraphView : GraphView
     {
         public GBQuestGraphView(){
+            AddManipulators();
             AddGridBackGround();
+            AddStyles();
+        }
+
+        private void AddManipulators(){
+            this.AddManipulator(new ContentDragger());
+            this.AddManipulator(new ContentZoomer());   
         }
 
         private void AddGridBackGround(){
             GridBackground gridBackground = new GridBackground();
-
             gridBackground.StretchToParentSize();
-
             Insert(0, gridBackground);
+        }
+
+        private void AddStyles(){
+            StyleSheet styleSheet = (StyleSheet) EditorGUIUtility.Load("GBQuestSystem/QuestGraphStyles.uss");
+            styleSheets.Add(styleSheet);
         }
     }
 
--- Assets/GBQuestSystem/Editor/GBQuestGraphWindow.cs
+++ Assets/GBQuestSystem/Editor/GBQuestGraphWindow.cs
@@ -10,19 +10,22 @@ namespace GBQuestSys.Windows
         [MenuItem("Window/GBQuestSystem/Quest Graph")]
         public static void Open(){
             GBQuestGraphWindow editorWindow = GetWindow<GBQuestGraphWindow>("QuestEditorWindow");
-
         }
 
         private void OnEnable() {
             AddGraphView();
+            AddStyles();
         }
 
         private void AddGraphView(){
             GBQuestGraphView graphView = new GBQuestGraphView();
-
             graphView.StretchToParentSize();
-
             rootVisualElement.Add(graphView);
+        }
+
+        private void AddStyles(){
+            StyleSheet styleSheet = (StyleSheet) EditorGUIUtility.Load("GBQuestSystem/GBQuestSysVariables.uss");
+            rootVisualElement.styleSheets.Add(styleSheet);
         }
     }
 }
